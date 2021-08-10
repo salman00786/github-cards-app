8b5207e880eeedc65475f7c1d53c604a9fa2de4a
Muhammad Salman Iqbal <salmaniqbal00786@gmail.com>
2021-08-09 22:57:46 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import './App.css';
+import React from 'react';
+import axios from 'axios';
+ 
+const testData = [
+  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"}
+]
+
+
+
+
+const CardList = (props) =>(
+ <div>
+     {props.profiles.map(profiless =><Card{...profiless}/>)}
+ </div>
+)
+
+class Card extends React.Component{
+  render(){
+    let profile = this.props;
+    return(
+      <div style={{textAlign:"center"}}>
+      <div className="github-profile"></div>
+      <img src={profile.avatar_url}/>
+      <div className="name">{profile.name}</div>
+      <div className="company">{profile.company}</div>
+      </div>
+    )
+  }
+}
+
+class Form extends React.Component{
+    state = {userName :""}
+    handleSubmit= async (event)=>{
+     event.preventDefault();
+     const resp= await axios.get(`https://api.github.com/users/${this.state.userName}`)
+   this.props.onSubmit(resp.data);
+   this.setState({userName:""});
+    
+     
+    }
+  render() {       
+    return(
+      <form action="" onSubmit={this.handleSubmit}>
+        <input type="text"
+        placeholder="Enter Username..." 
+        value={this.state.userName}
+        onChange = {event => this.setState({userName:event.target.value})}
+        
+       
+       
+        required/>
+        <button className="submitBtn">Add Card</button>
+      </form>
+    )
+  }
+}
+
+
+
+class App extends React.Component{
+  state ={
+    profiles : [],
+  };
+
+
+  addNewProfile = (profileData) =>{
+    this.setState(prevState => ({ profiles:[...prevState.profiles, profileData],}))
+    console.log("test",profileData)
+  }
+  render(){
+    return(
+      <div>
+      <div className="header">{this.props.title}</div>
+      <Form onSubmit={this.addNewProfile}/>
+      <CardList profiles={this.state.profiles}/>
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
